55fb5ef04b409db52b306b36a9b41f37ffc41518
NicholasGibsonUVM <nsgibson@uvm.edu>
2021-12-08 10:11:07 -0500

    added a way to choose users from the admin site

---
+++ admin/displayUser.php
@@ -0,0 +1,29 @@
+<?php 
+include 'top.php';
+$user = (isset($_GET['usr'])) ? htmlspecialchars($_GET['usr']) : '';
+$selectRecipes = 'SELECT `pmkRecipeName` FROM `tblRecipe` WHERE `fpkUsername`="' . $user . '"';
+$recipes = $thisDatabaseReader->select($selectRecipes);
+$recipeArray = array();
+$counter = 0;
+foreach ($recipes as $recipe) {
+    $recipe = new Recipe($recipe['pmkRecipeName']);
+    $recipeMainArray = $recipe->getMain();
+    $recipeArray[$counter] = $recipeMainArray;
+    $counter++;
+}
+?>
+
+<main>
+    <h1><?php print $user; ?>'s Recipes</h1>
+    <section class=returnedRecipes>
+        <?php 
+        recipePreviewAdmin($recipeArray);
+        ?>
+    </section>
+
+</main>
+
+
+<?php 
+include '../templates/footer.php';
+?>
